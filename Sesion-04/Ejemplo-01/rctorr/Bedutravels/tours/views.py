fad5a98039a9d8a42b2c4b6664215dd8b3bab6e0
Ricardo Torres <rictor@cuhrt.com>
2021-10-02 11:29:41 -0500

    Se agrega el uso de las vistas de LoginView de Django

--- Sesion-04/Ejemplo-01/rctorr/Bedutravels/tours/views.py
+++ Sesion-04/Ejemplo-01/rctorr/Bedutravels/tours/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .models import Tour
@@ -13,28 +12,3 @@ def index(request):
 
 	return render(request, "tours/index.html", {"tours":tours})
 
-def login_user(request):
-    """ Atiende las peticiones de GET y POST /login/ """
-
-    if request.method == "POST":
-        # Se obtienen los datos del formulario
-        user = request.POST["username"]
-        passwd = request.POST["password"]
-        next_ = request.GET.get("next", "/")
-        user_obj = authenticate(username=user, password=passwd)
-        if user_obj != None:
-            # Tenemos usuario válido, redireccionamos a index
-            login(request, user_obj)  # crea la sesión usando cookies
-            return redirect(next_)
-        else:
-            # Usuario malo
-            msg = "Datos incorrectos, intente de nuevo!"
-    else:
-        # Si no hay datos POST entonces es GET y enviamos formulario
-        msg = ""
-
-    return render(request, "registration/login.html",
-        {
-            "msg":msg,
-        }
-    )
